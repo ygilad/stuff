3ba2db455586063afb559280ea93f233b59e4d5d
yuval Gilad <ygilad@gmail.com>
2019-04-30 17:39:06 +0300

    Create choco.bat

---
+++ choco.bat
@@ -0,0 +1,9 @@
+REM From https://www.facebook.com/groups/hitechproblems/permalink/724338037968588/
+
+choco install --accept-license -y 7zip notepadplusplus
+choco install --accept-license -y mremoteng teamviewer putty.install
+choco install --accept-license -y googlechrome firefox filezilla
+choco install --accept-license -y ditto everything
+choco install --accept-license -y pdfxchangeeditor 
+choco install --accept-license -y sysinternals
+choco install --accept-license -y vlc keepass mp3tag renamer
